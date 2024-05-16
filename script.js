6790f6ef8c18b436f6b9726cd6a8dff9b69f390e
bprasad156 <156602266+bprasad156@users.noreply.github.com>
2024-05-16 17:54:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function fibonacci(num) {
     let fib = [0, 1];
     for (let i = 2; i < n; i++) {
-        fib[i] = fib[i - 1] + fib[i - 2];
+        fib[i] = fibonacci[i - 1] + fibonacci[i - 2];
     }
     return fib.slice(0, num);
 }
-console.log(fibonacciSeries(num));
 
 module.exports = fibonacci;
