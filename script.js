deb9d4a52655404a707e81fc72e72edf8e7d88d4
bprasad156 <156602266+bprasad156@users.noreply.github.com>
2024-05-16 17:20:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function fibonacci(num) {
+function fibonacci(num) 
+let a= 0;
+let b=1;
+console.log(a);
+console.log(b);
+for(let i=0;i<=num.length;i=i+1){
+	i=a+b
+	console.log(i)
+	a=b;
+	b=i;
+}
 // your code here
 }
 
